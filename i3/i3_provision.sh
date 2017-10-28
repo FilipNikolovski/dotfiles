227c62a3d91e6493c38919f6828b6127b3119ac6
Filip Nikolovski <filip.nikol92@gmail.com>
2017-10-28 19:52:12 +0200

    update

--- i3/i3_provision.sh
+++ i3/i3_provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+xrandr --newmode "1920x1080" 173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode eDP1 1920x1080
 xrandr && xrandr --output eDP1 --mode 1920x1080 --output HDMI1 --auto --right-of eDP1 
 
